2cf1a63056c3ea188302bc59818ed6dd9ae1a31d
ClaytonStealth <claytonses@yahoo.com>
2023-03-23 11:38:30 -0400

    added async await version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 import useAPI from "./hooks/useAPI";
 function App() {
-  return <div className='App App-header'>
-    <h1>Custom API Hooks</h1>
-  </div>;
+  console.log(useAPI("users", 4));
+  return (
+    <div className='App App-header'>
+      <h1>Custom API Hooks</h1>
+    </div>
+  );
 }
 
 export default App;
--- src/hooks/useAPI.js
+++ src/hooks/useAPI.js
@@ -1,6 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-const useAPI = () => {
+const useAPI = (route, id) => {
+  //fetch data
+  useEffect(() => {
+    const baseUrl = "https://jsonplaceholder.typicode.com";
+    fetch(`${baseUrl}/${route}/${id}`)
+      .then((response) => response.json())
+      .then((json) => console.log(json))
+      .catch((error) => console.log(error));
+  }, [route, id]);
+  //async await
+  // const fetchData = async () => {
+  //   const baseUrl = "https://jsonplaceholder.typicode.com";
+  //   const response = await fetch(`${baseUrl}/${route}/${id}`);
+  //   const data = await response.json();
+  //   return data;
+  // }
+  // fetchData();
   return <div>useAPI</div>;
 };
 
